2c5d66f767ea93b964c0ad9ccd1d05d0dcc407b3
Dichotomous247 <60080073+Dichotomous247@users.noreply.github.com>
2024-11-26 11:02:11 -0800

    some more edits

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 function currentBets() {
   window.location.href = "index.js";
 }
+
+const myModal = document.getElementById("myModal");
+const myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
