af597dc8b708fdb87867d54873c3fd31c91d7313
IH00711 <Caiharry2007@outlook.com>
2024-11-28 14:50:22 -0800

    add to index

--- js/createbet.js
+++ js/createbet.js
@@ -1,8 +1,16 @@
 document.getElementById('betForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const betTitle = document.getElementById('betTitle').value;
-    const betDescription = document.getElementById('betDescription').value;
     const bet = { title: betTitle, description: betDescription };
-    localStorage.setItem('newBet', JSON.stringify(bet));
-    window.location.href = 'index.html';
+    const betAmount = document.getElementById('betAmount').value;
+   
+    if(betTitle && bet && betAmount) {
+        const betDetails = {title: betTitle, bet: bet, amount: betAmount};
+        localStorage.setItem('newBet', JSON.stringify(bet));
+        window.location.href = 'index.html';
+    }
+    else{
+        alert('Please fill in all fields.');
+    }
+
 });
